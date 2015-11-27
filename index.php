8e7b3af9d297cd140f3e1732bdd5c877fe2355fd
nspanring <n.spanring@backbone.co.at>
2015-11-27 14:51:32 +0100

    BIG Update: 1.2 / Templates / Webservice / Login

--- index.php
+++ index.php
@@ -25,12 +25,15 @@
 /**
  * Front end of the Cap-php-library
  */
-
+	error_reporting(E_ERROR);
+	
 	require_once 'class/cap.form.class.php';
 	require_once 'lib/cap.create.class.php';
 	require_once 'lib/cap.write.class.php';
 	require_once 'lib/cap.convert.class.php';
 	require_once 'class/translate.class.php';
+	
+	$langs = new Translate();
 	
 	/**
    * encrypt and decrypt function for passwords
@@ -67,11 +70,90 @@
 	if(file_exists('conf/conf.php'))
 	{
 		include 'conf/conf.php';
-	
-		$langs = new Translate();		
 		if(! empty($_GET['lang'])) $conf->user->lang = $_GET['lang'];
 		$langs->setDefaultLang($conf->user->lang);		
 		$langs->load("main");	
+	}
+	}
+	else
+	{
+		$capfile = fopen('conf/conf.php', "w");
+		fwrite($capfile, "
+		<?php
+		$"."conf->lang['en-GB']                                   = 'english';
+		$"."conf->lang['ca']                                      = 'català';
+		$"."conf->lang['cs']                                      = 'ceština';
+		$"."conf->lang['da-DK']                                   = 'dansk';
+		$"."conf->lang['de-DE']                                   = 'deutsch';
+		$"."conf->lang['es-ES']                                   = 'español';
+		$"."conf->lang['et']                                      = 'eesti';
+		$"."conf->lang['eu']                                      = 'euskera';
+		$"."conf->lang['fr-FR']                                   = 'français';
+		$"."conf->lang['gl']                                      = 'galego';
+		$"."conf->lang['hr-HR']                                   = 'hrvatski';
+		$"."conf->lang['is']                                      = 'íslenska';
+		$"."conf->lang['it-IT']                                   = 'italiano';
+		$"."conf->lang['lt']                                      = 'lietuviu';
+		$"."conf->lang['lv']                                      = 'latviešu';
+		$"."conf->lang['hu-HU']                                   = 'magyar';
+		$"."conf->lang['mt']                                      = 'malti';
+		$"."conf->lang['nl-NL']                                   = 'nederlands';
+		$"."conf->lang['no']                                      = 'norsk';
+		$"."conf->lang['pl']                                      = 'polski';
+		$"."conf->lang['pt-PT']                                   = 'português';
+		$"."conf->lang['ro']                                      = 'româna';
+		$"."conf->lang['sr']                                      = '??????';
+		$"."conf->lang['sl']                                      = 'slovenšcina';
+		$"."conf->lang['sk']                                      = 'slovencina';
+		$"."conf->lang['fi-FI']                                   = 'suomi';
+		$"."conf->lang['sv-SE']                                   = 'svenska';
+		$"."conf->lang['el-GR']                                   = '????????';
+		$"."conf->lang['bg']                                      = 'bulgarian';
+		$"."conf->lang['mk']                                      = '??????????';
+		$"."conf->lang['name']                                    = '';
+		
+		$"."conf->select->lang['en-GB']                           = 1;
+		$"."conf->select->lang['ca']                              = 0;
+		$"."conf->select->lang['cs']                              = 0;
+		$"."conf->select->lang['da-DK']                           = 0;
+		$"."conf->select->lang['de-DE']                           = 1;
+		$"."conf->select->lang['es-ES']                           = 0;
+		$"."conf->select->lang['et']                              = 0;
+		$"."conf->select->lang['eu']                              = 0;
+		$"."conf->select->lang['fr-FR']                           = 0;
+		$"."conf->select->lang['gl']                              = 0;
+		$"."conf->select->lang['hr-HR']                           = 0;
+		$"."conf->select->lang['is']                              = 0;
+		$"."conf->select->lang['it-IT']                           = 0;
+		$"."conf->select->lang['lt']                              = 0;
+		$"."conf->select->lang['lv']                              = 0;
+		$"."conf->select->lang['hu-HU']                           = 0;
+		$"."conf->select->lang['mt']                              = 0;
+		$"."conf->select->lang['nl-NL']                           = 0;
+		$"."conf->select->lang['no']                              = 0;
+		$"."conf->select->lang['pl']                              = 0;
+		$"."conf->select->lang['pt-PT']                           = 0;
+		$"."conf->select->lang['ro']                              = 0;
+		$"."conf->select->lang['sr']                              = 0;
+		$"."conf->select->lang['sl']                              = 0;
+		$"."conf->select->lang['sk']                              = 0;
+		$"."conf->select->lang['fi-FI']                           = 0;
+		$"."conf->select->lang['sv-SE']                           = 0;
+		$"."conf->select->lang['el-GR']                           = 0;
+		$"."conf->select->lang['bg']                              = 0;
+		$"."conf->select->lang['mk']                              = 0;
+		$"."conf->trans['en_US']                                  = 'english';
+		$"."conf->trans['de_DE']                                  = 'deutsch';
+		$"."conf->trans['fr_FR']                                  = 'français';
+		$"."conf->trans['es_ES']                                  = 'Español';
+		?>
+		");
+		fclose($capfile);
+		$conf->user->lang = 'en_US';
+		$langs->setDefaultLang($conf->user->lang);		
+		$langs->load("main");	
+		
+		header("Refresh:0");
 	}
 	
 	// METEOALARM WEBSERVICE ---
